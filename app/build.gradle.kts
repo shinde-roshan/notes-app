fa458c95bc4db05333860eeb046c56f25b2c5457
roshan <roshan.shinde164@gmail.com>
2025-01-14 23:18:08 +0530

    libraries updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.room.runtime)
+    implementation(libs.androidx.ui.text.google.fonts)
     ksp(libs.room.compiler)
     implementation(libs.room.ktx)
     testImplementation(libs.junit)
