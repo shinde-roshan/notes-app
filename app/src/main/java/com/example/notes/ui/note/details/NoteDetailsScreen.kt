eac646aec3bf20840f7833ec7c5ad67a63814216
roshan <roshan.shinde164@gmail.com>
2025-01-23 00:20:14 +0530

    NoteDetailsScreen ui

--- app/src/main/java/com/example/notes/ui/note/details/NoteDetailsScreen.kt
+++ app/src/main/java/com/example/notes/ui/note/details/NoteDetailsScreen.kt
@@ -1,15 +1,23 @@
 package com.example.notes.ui.note.details
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.notes.AppBar
+import com.example.notes.R
 import com.example.notes.ui.AppViewModelProvider
+import com.example.notes.utils.toDateStr
 
 @Composable
 fun NoteDetailsScreen(
@@ -31,11 +39,31 @@ fun NoteDetailsScreen(
         },
         modifier = modifier.fillMaxSize()
     ) { innerPadding ->
-        Text(
-            text = "Note Details: $noteId",
-            modifier = Modifier
+        Column(
+            modifier = modifier
                 .padding(innerPadding)
-                .fillMaxSize()
-        )
+                .verticalScroll(rememberScrollState())
+        ) {
+            Text(
+                text = uiState.value.note?.title ?: "",
+                style = MaterialTheme.typography.headlineSmall,
+                modifier = Modifier
+                    .padding(horizontal = dimensionResource(R.dimen.dp_16))
+            )
+            Text(
+                text = uiState.value.note?.timestamp?.let { toDateStr(it) } ?: "",
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier
+                    .align(Alignment.End)
+                    .padding(dimensionResource(R.dimen.dp_16))
+            )
+            Text(
+                text = uiState.value.note?.content ?: "",
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = dimensionResource(R.dimen.dp_16))
+            )
+        }
     }
 }
