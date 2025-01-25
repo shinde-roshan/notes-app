7b4433dc3a583627a595e659903e20441a35dfcb
roshan <roshan.shinde164@gmail.com>
2025-01-25 13:19:06 +0530

    CreateUiState extension function added to convert it to Note object

--- app/src/main/java/com/example/notes/ui/note/create/CreateNoteUiState.kt
+++ app/src/main/java/com/example/notes/ui/note/create/CreateNoteUiState.kt
@@ -1,9 +1,17 @@
 package com.example.notes.ui.note.create
 
 import androidx.annotation.StringRes
+import com.example.notes.database.Note
 
 data class CreateNoteUiState(
     val titleText: String = "",
     val contentText: String = "",
     @StringRes val titleErrorMsgRes: Int? = null
 )
+
+fun CreateNoteUiState.toNote(): Note {
+    return Note(
+        title = titleText,
+        content = contentText
+    )
+}
