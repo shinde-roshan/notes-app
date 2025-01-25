e50ee9ff78ed0471d02584b30bc8a0c217e5a143
roshan <roshan.shinde164@gmail.com>
2025-01-25 11:37:34 +0530

    added windowSoftInputMode and ime padding

--- app/src/main/java/com/example/notes/ui/note/create/CreateNoteScreen.kt
+++ app/src/main/java/com/example/notes/ui/note/create/CreateNoteScreen.kt
@@ -4,8 +4,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -37,6 +40,8 @@ fun CreateNoteScreen(
             modifier = Modifier
                 .padding(innerPadding)
                 .background(MaterialTheme.colorScheme.surface)
+                .verticalScroll(state = rememberScrollState())
+                .imePadding()
         ) {
             NoteTitle(
                 text = uiState.titleText,
