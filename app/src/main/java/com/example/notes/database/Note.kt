87460b2763bb60122dece24d2d8ea6e1c3eebd08
roshan <roshan.shinde164@gmail.com>
2025-01-26 16:11:04 +0530

    default 0 is considered for auto increment

--- app/src/main/java/com/example/notes/database/Note.kt
+++ app/src/main/java/com/example/notes/database/Note.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "notes")
 data class Note(
-    @PrimaryKey(autoGenerate = true) val id: Int = -1,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val title: String,
     val content: String,
     val color: String = "",
