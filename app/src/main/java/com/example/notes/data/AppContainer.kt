59cc631a35c32bd8f5adc0f725936d73bc2cd20c
roshan <roshan.shinde164@gmail.com>
2025-02-16 09:01:55 +0530

    data store instance created, UserPreferencesRepository instance created for dependency injection

--- app/src/main/java/com/example/notes/data/AppContainer.kt
+++ app/src/main/java/com/example/notes/data/AppContainer.kt
@@ -1,6 +1,9 @@
 package com.example.notes.data
 
 import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import com.example.notes.database.NotesDatabase
 
 /**
@@ -8,10 +11,19 @@ import com.example.notes.database.NotesDatabase
  */
 interface AppContainer {
     val notesRepository: NotesRepository
+    val userPreferencesRepository: UserPreferencesRepository
 }
+
+private const val NOTES_USER_PREF_NAME = "notes_user_preferences"
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
+    name = NOTES_USER_PREF_NAME
+)
 
 class AppDataContainer(private val context: Context): AppContainer {
     override val notesRepository: NotesRepository by lazy {
         NotesRepository(NotesDatabase.getInstance(context).notesDao())
+    }
+    override val userPreferencesRepository: UserPreferencesRepository by lazy {
+        UserPreferencesRepository(context.dataStore)
     }
 }
